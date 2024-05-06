7219ee60ad312189a19fb5237bab26d867d614a7
unknown <secure@profility.slmail.me>
2024-05-06 18:59:22 +0800

    add confirmation & others

--- errors.py
+++ errors.py
@@ -5,6 +5,12 @@ class PassManError(Exception):
         print(message)
         sys.exit(1)
 
+class PassManConfirmation():
+    def __init__(self, message):
+        confirmation = input(f"{message} [y/N]: ")
+        if confirmation.lower() == "n":
+            sys.exit(0)
+
 def NotInitialized():
     """If passman vault is not yet initialized"""
 
@@ -18,9 +24,19 @@ def AlreadyInitialized():
 def EntryNotFound(entry):
     """If login entry is not found"""
 
-    PassManError(f"Entry {entry} not found, exiting...")
+    PassManError(f"Entry '{entry}' not found, exiting...")
 
 def GPGIdNotFound():
     """If GPG Id is not found"""
 
-    PassManError("GPG ID file does not exist, please run `passman init`")
+    PassManError("GPG ID file does not exist, please run `passman init`")\
+    
+def EntryAlreadyExists(entry):
+    """If login entry already exists"""
+
+    PassManConfirmation(f"Entry '{entry}' already exists, overwrite?")
+
+def DecryptionError():
+    """If decryption fails"""
+
+    PassManError("Decryption failed; probably due to wrong passphrase, exiting...")
